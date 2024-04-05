c3a815b345dbbaf56f4c4bc95aef00724d898524
leegeonhwi0 <leegeonhwi0@github.com>
2024-04-05 12:05:46 +0900

    add output

--- modules/ec2/output.tf
+++ modules/ec2/output.tf
@@ -19,3 +19,15 @@ output "kube_controller_ips_c" {
 output "kube_worker_ips" {
   value = aws_instance.kube_worker[*].private_ip
 }
+
+output "kube_worker_ips_c" {
+  value = aws_instance.kube_worker_c[*].private_ip
+}
+
+output "haproxy1_ips" {
+  value = aws_instance.haproxy1[*].private_ip
+}
+
+output "haproxy2_ips" {
+  value = aws_instance.haproxy2[*].private_ip
+}
